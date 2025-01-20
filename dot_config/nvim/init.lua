5f784034ca21e26cfa5d152f47f856553e8887fe
Pierrelefort <pierre.lefort@epita.fr>
2025-01-20 23:47:43 +0100

    fix: remove commitizen gitmoji

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -17,36 +17,3 @@ end
 
 require "lazy_setup"
 require "polish"
-
--- custom setup init.lua
-
-require("toggleterm").setup({
-  direction = "float", -- Floating terminal
-})
-
--- Commitizen commit
-vim.keymap.set("n", "<Leader>gzc", function()
-  -- Check if there are staged files
-  local handle = io.popen("git diff --cached --name-only")
-  local staged_files = handle:read("*a")
-  handle:close()
-
-  if #staged_files == 0 then
-    -- No files staged for commit, show a warning message
-    vim.notify(
-      "No files staged for commit.\n\nUse 'git add <file>' to stage files.",
-      vim.log.levels.WARN,
-      { title = "Commitizen Warning" }
-    )
-
-    return
-  end
-
-  require("toggleterm.terminal").Terminal:new({
-    cmd = "cz commit",
-    direction = "float",
-    close_on_exit = true,
-  }):toggle()
-end, { noremap = true, silent = true, desc = "Run Commitizen (cz commit)" })
-
--- end custom setup
